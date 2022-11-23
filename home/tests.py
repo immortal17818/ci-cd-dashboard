e0362f52944b086a6f10ff9e5c932f6ea13d1276
Shivam <shivam.myhealthcare@outlook.com>
2022-11-23 19:07:47 +0530

    test-added

--- home/tests.py
+++ home/tests.py
@@ -1,15 +1,1 @@
 from django.test import TestCase
-from django.contrib.auth.models import User
-
-
-# Create your tests here.
-
-class UserTestCase(TestCase):
-    def test_user(self):
-        username = 'Thakur'
-        password = 'Shivam@17818'
-        u = User(username=username)
-        u.set_password(password)
-        u.save()
-        self.assertEqual(u.username, username)
-        self.assertTrue(u.check_password(password))
